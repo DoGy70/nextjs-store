4910359cf023ed4a5876dba0e8ad631d4bf7872c
Miroslav <pomanovmiroslav@gmail.com>
2024-06-18 13:40:17 +0300

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+function AboutPage() {
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          store
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sapiente
+        iste dicta impedit culpa cumque fugit ea unde, voluptatem voluptates
+        dignissimos quaerat, numquam est necessitatibus veritatis expedita ad.
+        Ipsa placeat voluptatibus beatae impedit nisi? Placeat corrupti sapiente
+        nemo repellat quis.
+      </p>
+    </section>
+  );
+}
+export default AboutPage;
